35187745fd9b0340c466923a73a6c06a4e208d89
mark-marshall <mark.marshallgp@gmail.com>
2019-03-22 10:11:35 +0000

    [STRETCH] Added CRA client and CORS to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = express();
 const projectRoutes = require('./routes/projects');
 const actionRoutes = require('./routes/actions');
 
+server.use(cors());
 server.use(projectRoutes);
 server.use(actionRoutes);
 
