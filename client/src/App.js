8ce6cc5dfcdc534aff7d858f65a0d8d51892b4bd
mark-marshall <mark.marshallgp@gmail.com>
2019-03-22 10:57:17 +0000

    [STRETCH] Added basic CRA serving projects on mount

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+const projectUrl = 'http://localhost:4000/api/projects';
 
 class App extends Component {
+  state = {
+    projects: [],
+    error: '',
+  };
+
+  componentDidMount() {
+    axios
+      .get(projectUrl)
+      .then(projects => {
+        this.setProjects(projects.data);
+      })
+      .catch(error => {
+        this.setError(error.message);
+      });
+  }
+
+  setProjects = projects => {
+    this.setState({
+      projects,
+    })
+  }
+
+  setError = error => {
+    this.setState({
+      error,
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (this.state.error) {
+      return <div>We got an error over here: {this.state.error};</div>;
+    } else {
+      return (
+        <div>
+          Hello World
+          <ul>
+            {this.state.projects.map(project => (
+              <li key={project.id}>
+                <h5>Name: {project.name}</h5>
+                <p>Description: {project.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 
 const projectRoutes = require('./routes/projects');
 const actionRoutes = require('./routes/actions');
 
 server.use(cors());
+server.use(express.json());
+
 server.use(projectRoutes);
 server.use(actionRoutes);
 
